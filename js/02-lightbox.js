79906dc6bde95868897035901542b7addf4bde1e
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-14 17:26:34 +0300

    finished 02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imagesContainer = document.querySelector('.gallery');
+const imagesCardsMarkup = createImagesCardsMarkup(galleryItems);
+
+imagesContainer.insertAdjacentHTML('beforeend', imagesCardsMarkup);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+function createImagesCardsMarkup(images) {
+  return images
+    .map(image => {
+      return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${image.original}">
+                    <img 
+                    class="gallery__image" 
+                    src="${image.preview}" 
+                    alt="${image.description}"
+                    />
+                </a>
+            </div>
+        `;
+    }).join('');
+}
+
