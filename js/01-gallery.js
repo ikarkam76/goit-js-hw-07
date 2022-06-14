5c6f383123b54b2f00a88b3842b8cf27f4ce887a
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-14 14:04:28 +0300

    finished 01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const imagesContainer = document.querySelector('.gallery')
+const imagesCardsMarkup = createImagesCardsMarkup(galleryItems);
+
+imagesContainer.insertAdjacentHTML('beforeend', imagesCardsMarkup);
+
+imagesContainer.addEventListener('click', onImageCardClick);
+
+function onImageCardClick(evt) {
+    evt.preventDefault();
+    const currentActiveImage = evt.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+            <div class="modal">
+                <img
+                    src= ${currentActiveImage}
+                />
+            </div>
+        `);
+
+        instance.show();
+
+        document.addEventListener('keydown', event => {
+            if (event.code === 'Escape') {
+            instance.close();
+            }
+        }
+    )
+};
+
+function createImagesCardsMarkup(images) {
+    return images.map(image => {
+        return `
+            <div class="gallery__item">
+                <a class="gallery__link" href="${image.original}">
+                    <img
+                        class="gallery__image"
+                        src="${image.preview}"
+                        data-source="${image.original}"
+                        alt="${image.description}"
+                    />
+                </a>
+            </div>
+        `;
+    }).join('');
+}
+
